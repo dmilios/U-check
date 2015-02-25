218b4afa8210c8dd295bd1b61b25a70e99244b0a
dmilios <dmilios@staffmail.ed.ac.uk>
2015-02-25 17:25:11 +0000

    unckeck added 1.1

--- Ucheck/src/cli/Log.java
+++ Ucheck/src/cli/Log.java
@@ -5,28 +5,28 @@ public abstract class Log {
 	private int warnings = 0;
 	private int errors = 0;
 
-	public abstract void print(String s);
-
-	public abstract void println(String s);
-
-	public abstract void println();
-
-	final public int getWarnings() {
+	public int getWarnings() {
 		return warnings;
 	}
-	
-	final public int getErrors() {
+
+	public int getErrors() {
 		return errors;
 	}
-	
+
 	final public void printWarning(String s) {
 		warnings++;
-		println("Warning: " + s);
+		this.println(s);
 	}
 
 	final public void printError(String s) {
 		errors++;
-		println("Error: " + s);
+		this.println(s);
 	}
+
+	abstract public void print(String s);
+
+	abstract public void println(String s);
+
+	abstract public void println();
 
 }
